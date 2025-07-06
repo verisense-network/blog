5decd5feb638f94650e984e96b7f38b41356a4f8
0xed1s0n <0xed1s0n@proton.me>
2025-07-06 20:27:20 +0800

    init

---
+++ pelicanconf.py
@@ -0,0 +1,31 @@
+AUTHOR = "Edison Zhang"
+SITENAME = "Verisense Blog"
+SITEURL = ""
+
+PATH = "content"
+
+TIMEZONE = "Asia/Shanghai"
+
+DEFAULT_LANG = "en"
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+    ("Verisense", "https://www.verisense.network/"),
+    ("Github", "https://github.com/verisense-network/"),
+    ("Docs", "https://docs.verisense.network/"),
+)
+
+# Social widget
+SOCIAL = (("X", "https://x.com/veri_sense"),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
